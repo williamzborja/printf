b4225cf3a36823de92f6bea5d166a3c4c47d8532
william rodriguez <rodriguezborjawilliam@gmail.com>
2021-03-17 20:54:27 -0500

    handle malloc return in putchar

--- _putchar.c
+++ _putchar.c
@@ -9,14 +9,14 @@
 */
 int _putchar(char c, char *buffer, int *size)
 {
-	size_t i;
-
 	if (*size == BUFFER_SIZE)
 	{
 		write(1, buffer, BUFFER_SIZE);
-		for (i = 0; i < BUFFER_SIZE; i++)
-			buffer = '\0';
+		free(buffer);
 		*size = 0;
+		buffer = malloc(BUFFER_SIZE);
+		if (!buffer)
+			return (0);
 	}
 	buffer[*size] = c;
 	*size += 1;
